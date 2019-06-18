a6e6864eeb6eaf1763848add59a568ce72d3e837
Pawan Kumar <pawamkumar17264@gmail.com>
2019-06-18 09:21:36 +0530

    refatored

--- app.js
+++ app.js
@@ -19,44 +19,43 @@ firebase.initializeApp(firebaseConfig);
 var dat_c;
 var dat_r;
 var newDate = new Date();
-fs.exists("C:/UltraTect_assets/revenue",function(exists) {
+fs.exists("C:/UltraTect_assets", function (exists) {
     console.log("file exists ? " + exists);
-    if (!exists){
+    if (!exists) {
         fs.mkdir("C:/UltraTect_assets/revenue", (err) => {
-            if (err) throw err});
-        }
-});
-fs.exists("C:/UltraTect_assets/capax",function(exists) {
-    console.log("file exists ? " + exists);
-    if (!exists){
+            if (err) throw err
+        });
+    }else{
+        var messagesRef = firebase.database().ref('Revenue Details');
+        messagesRef.on("value", function (data) {
+            dat_r = JSON.stringify(data);
+            console.log(dat_r);
+            fs.writeFile('C:/UltraTect_assets/revenue/Output-' + newDate.getDate().toString() + "-" +
+                (newDate.getMonth() + 1).toString() + "-" + newDate.getFullYear().toString() + ".json",
+                dat_r, (err) => {
+                    if (err) throw err;
+                })
+        
+        });
+    }
+    if (!exists) {
         fs.mkdir("C:/UltraTect_assets/capax", (err) => {
-            if (err) throw err});
-        }
+            if (err) throw err
+        });
+    }else{
+        var messagesRef = firebase.database().ref('Capax Details');
+        messagesRef.on("value", function (data) {
+            dat_c = JSON.stringify(data);
+            console.log(dat_c);
+            fs.writeFile('C:/UltraTect_assets/capax/Output-' + newDate.getDate().toString() + "-" +
+                (newDate.getMonth() + 1).toString() + "-" + newDate.getFullYear().toString() + ".json",
+                dat_c, (err) => {
+                    if (err) throw err;
+                })
+        
+        });
+    }
 });
-var messagesRef = firebase.database().ref('Revenue Details');
-messagesRef.on("value", function (data) {
-     dat_r = JSON.stringify(data);
-     console.log(dat_r);
-    fs.writeFile('C:/UltraTect_assets/revenue/Output-' + newDate.getDate().toString() + "-" +
-    (newDate.getMonth()+1).toString() + "-" + newDate.getFullYear().toString() + ".json", 
-    dat_r , (err) => {
-        if (err) throw err;
-    })
-    
-});
-
-var messagesRef = firebase.database().ref('Capax Details');
-messagesRef.on("value", function (data) {
-     dat_c = JSON.stringify(data);
-     console.log(dat_c);
-    fs.writeFile('C:/UltraTect_assets/capax/Output-' + newDate.getDate().toString() + "-" +
-    (newDate.getMonth()+1).toString() + "-" + newDate.getFullYear().toString() + ".json", 
-    dat_c , (err) => {
-        if (err) throw err;
-    })
-    
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/js"));
