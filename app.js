aefbb55027868a9e2aa6d27444d4fbaf96e22859
Pawan Kumar <pawamkumar17264@gmail.com>
2019-06-26 18:06:07 +0530

    fixed edit data part

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
 const port = 3000;
+
 var json = require('./assets/example_1.json');
 
 //mongoose Setup
@@ -34,6 +35,7 @@ app.post('/capax_save', function (req, res) {
   var model = req.body.exampleModel;
   var modelDesp = req.body.exampleModelDescp;
   var summit = req.body.exampleUpdateSummit;
+  console.log(summit);
 
   var data = {
     "serialNumber": serialNumber,
@@ -49,7 +51,7 @@ app.post('/capax_save', function (req, res) {
     "modelDesp": modelDesp,
     "summit": summit
   }
-  db.collection('Capax').insertOne(json, function (err, collection) {
+  db.collection('Capax').insertOne(data, function (err, collection) {
     if (err) throw err;
     console.log("Record inserted Successfully");
     res.redirect("/capax");
@@ -61,8 +63,13 @@ app.post('/capax_search', function (req, res) {
   console.log(serialnum);
   db.collection('Capax').findOne({ "serialNumber": serialnum })
     .then(function (result) {
+      if (result == null){
+        console.log('No Data found');
+     
+      }else{
       console.log(result);
       datas = result;
+      }
       res.redirect('/c');
     })
 })
@@ -91,6 +98,8 @@ app.post('/capax_del', function (req, res) {
 })
 
 app.get('/c', function (req, res) {
+  console.log(datas.summit)
+  console.log(datas.modelDesp);
   res.render('search_c', {
     exampleserailNumber: datas.serialNumber,
     exampleSapCode: datas.sapCode,
@@ -107,10 +116,8 @@ app.get('/c', function (req, res) {
   });
 })
 
-app.post('/capax_ed', function (req, res) {
-  res.redirect('/capax_edit');
-})
-app.get('/capax_edit', function (req, res){
+app.post('/capax_edit', function (req, res){
+  console.log(req.body) 
   res.render('capax_edit', {
     exampleserailNumber: datas.serialNumber,
     exampleSapCode: datas.sapCode,
@@ -125,11 +132,41 @@ app.get('/capax_edit', function (req, res){
     exampleModelDescp: datas.modelDesp,
     exampleUpdateSummit: datas.summit
   });
+  var serialNumber = req.body.exampleserailNumber;
+  var sapCode = req.body.exampleSapCode;
+  var materialCode = req.body.exampleMaterialCode;
+  var materialQuantity = req.body.exampleMaterialQuantity;
+  var poDate = req.body.examplepoDate;
+  var poNum = req.body.examplepoNumber;
+  var invoiceDate = req.body.exampleInvoiceDate;
+  var invoiceNumber = req.body.exampleInvoiceNumber;
+  var receiveDate = req.body.exampleRecieveDate;
+  var model = req.body.exampleModel;
+  var modelDesp = req.body.exampleModelDescp;
+  var summit = req.body.exampleUpdateSummit;
+  console.log(summit);
+
+  var data = {
+    "serialNumber": serialNumber,
+    "sapCode": sapCode,
+    "materialCode": materialCode,
+    "materialQuantity": materialQuantity,
+    "poDate": poDate,
+    "poNum": poNum,
+    "invoiceDate": invoiceDate,
+    "invoiceNumber": invoiceNumber,
+    "receiveDate": receiveDate,
+    "model": model,
+    "modelDesp": modelDesp,
+    "summit": summit
+  }
+  db.collection('Capax').insertOne(data, function (err, collection) {
+    if (err) throw err;
+    console.log("Record inserted Successfully");
+    res.redirect("/capax");
+  });
 })
 
-app.post('/capax_editsave', function (req, res){
-  //Wil save the edited data here, I will continue here
-})
 app.get('/', (req, res) => res.render('index'));
 app.get('/index', (req, res) => res.render('index'));
 app.get('/capax', (req, res) => res.render('capax'));
@@ -138,21 +175,6 @@ app.get('/search_c', (req, res) => res.render('search_c'));
 app.get('/search_r', (req, res) => res.render('search_r'));
 app.get('/r'), (req, res) => res.render('r');
 
-// app.get("/check/:id", (req, res) => {
-//     fs.writeFile("file.txt", (req.params.id), function (err) {
-//         if (err) {
-//             return console.log(err);
-//         }
-
-//         console.log("The file was saved!");
-//     });
-//     res.send(req.params.id);
-// })
-
-
-
-// HEROKU
-// app.listen(process.env.PORT, process.env.IP, () => console.log("Server started ..."))
 
 // LOCAL
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
